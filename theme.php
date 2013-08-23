1da51f8e69eb2c8a13e2c340eed09c37ccb18a85
Kaj <kaj@edocsyl.ch>
2013-08-23 12:58:17 +0200

    Test .9

--- theme.php
+++ theme.php
@@ -2,6 +2,8 @@
 require 'umfrage.class.php';
 $u = new umfrage('u_umfragen.dat');
 $umfragen = $u->rUmfragen();
+
+print_r($_POST);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,6 +31,9 @@ $umfragen = $u->rUmfragen();
 
     <div class="container">
 	<span class="span8">
+	
+	<form action="" method="POST">
+	
 	<div class="page-header">
           <h1><?php echo '#'.$umfragen[0]['id'] . ' ' . $umfragen[0]['titel'] ?></h1>
         </div>
@@ -45,7 +50,7 @@ $umfragen = $u->rUmfragen();
 		<strong>#<?php echo $i ?></strong> <?php echo $frage ?>
 		</div>
 		<div class="span3">
-		 <textarea id="message" name="message" class="span5" placeholder="Ihre Antwort" rows="3" maxlength="190"></textarea>
+		 <textarea id="message" name="answer<?php echo $i ?>" class="span5" placeholder="Ihre Antwort" rows="3" maxlength="190"></textarea>
 		</div>
 		</div>
 		<hr>
@@ -63,7 +68,6 @@ $umfragen = $u->rUmfragen();
 		 <input type="checkbox" class="myClass" value="yes" id="answer" name="answer" data-label="Facebook"/>
 		 <input type="checkbox" class="myClass" value="yes" id="answer" name="answer" data-label="Google+"/>
 		 <input type="checkbox" class="myClass" value="yes" id="answer" name="answer" data-label="Netlog"/>
-		 <input type="checkbox" class="myClass" value="yes" id="answer" name="answer" data-label="Lovoo"/>
 			
 		</div>
 		</div>
@@ -72,27 +76,30 @@ $umfragen = $u->rUmfragen();
 		<div class="controls">
 		<button href="#submitSurvey" type="submit" data-toggle="modal" class="btn btn-primary input-medium pull-right">Weiter</button>
 		</div>
+		
+		
+		<div id="submitSurvey" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&#x2716;</button>
+				<h3 id="myModalLabel">Umfrage beenden</h3>
+			</div>
+			<div class="modal-body">
+				<p>
+				<input id="name" name="name" type="text" placeholder="Ihr Name">
+				<input id="email" name="email" type="email" placeholder="Ihre E-Mail Adresse">
+			</div>
+			<div class="modal-footer">
+				<button class="btn" data-dismiss="modal" aria-hidden="true">Schliessen</button>
+				<button class="btn btn-primary" type="submit">Umfrage beenden</button>
+			</div>
+		</div>
+	</form>
 	</span>
     </div> <!-- /container -->
 	
 	
 	
-	<div id="submitSurvey" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-header">
-			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&#x2716;</button>
-			<h3 id="myModalLabel">Umfrage beenden</h3>
-		</div>
-		<div class="modal-body">
-			<p>
-			<input id="name" name="name" type="text" class="span3" placeholder="Ihr Name">
-			<input id="email" name="email" type="email" class="span3" placeholder="Ihre E-Mail Adresse">
-		</div>
-		<div class="modal-footer">
-			<button class="btn" data-dismiss="modal" aria-hidden="true">Schliessen</button>
-			<button class="btn btn-primary">Umfrage beenden</button>
-		</div>
-	</div>
-
+	
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
@@ -101,8 +108,6 @@ $umfragen = $u->rUmfragen();
     <script src="js/bootstrap-maxlength.js"></script>
     <script src="js/prettyCheckable/prettyCheckable.js"></script>
     <script>
-	
-	
 	$().ready(function(){
 		
 		$('textarea#message').maxlength({
