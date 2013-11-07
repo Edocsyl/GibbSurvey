02936c601d821db07c1781c745033cbaad194abc
Kaj <kaj@vmWP1>
2013-11-07 13:16:41 +0100

    asd

--- umfrage.class.php
+++ umfrage.class.php
@@ -36,6 +36,71 @@
 
 			return $data;
 		}
+		
+		public function getUmfragen(){
+		
+			print_r($_POST);
+			
+			$data = array();
+			
+			$txt_file = file_get_contents("umfragen/u1.txt") or die("can't open file");
+			
+			$umfragefile = explode("\n", $txt_file);
+			
+			$fragen_count = 0;
+			
+			echo '<form action="index.php" method="post">';
+			
+			foreach($umfragefile as $line) {
+				//$line = nl2br($line);
+				$line = htmlentities($line);
+				$type = substr($line, 0, 2);
+			
+				switch($type) {
+					case "H;":
+						echo "<h1>" . substr($line, 2) . "</h1>";
+						break;
+							
+					case "D;":
+						echo "<h3>" . substr($line, 2) . "</h3>";
+						break;
+							
+					case "C;":
+						echo "<h4>" . substr($line, 2) . "</h4>";
+						break;
+							
+					case "Q;":
+						echo substr($line, 2);
+						echo "<input style=\"display:none;\" checked=\"checked\" type=\"radio\" name=\"f$fragen_count\" value=\"0\"> <input type=\"radio\" name=\"f$fragen_count\" value=\"1\"> 0% <input type=\"radio\" name=\"f$fragen_count\" value=\"2\"> 25% <input type=\"radio\" name=\"f$fragen_count\" value=\"3\"> 75% <input type=\"radio\" name=\"f$fragen_count\" value=\"4\"> 100% <br />";
+						$fragen_count++;
+						break;
+							
+					case "T;":
+						echo substr($line, 2);
+						break;
+							
+					default;
+					break;
+				}
+			
+			}
+			
+			echo '<input type="submit" value="Absenden"></form>';
+		}
+		
+		
+		public function getUmfragen($dir = 'umfragen'){
+			$umfragen = array();
+			if($handle = opendir($dir)){
+				while (false !== ($file = readdir($handle))) {
+					if(substr($file, 0, 1) == 'u'){
+						array_push($umfragen, $file);
+					}
+				}
+			}
+			return $umfragen;
+		}
+		
 	
 	}
 
