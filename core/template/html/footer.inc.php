7e9b4e7cdc7696d8e5d77ba97030a723154acea2
Kaj <kaj@vmWP1>
2014-01-05 15:34:52 +0100

    asd

--- core/template/html/footer.inc.php
+++ core/template/html/footer.inc.php
@@ -3,24 +3,18 @@
         <p><?= $this->_config['copyright']; ?></p>
       </div>
 	</div>
+	
     <!-- Le javascript
     ================================================== -->
     <script src="<?= $this->_config['js']; ?>/jquery-1.10.2.min.js"></script>
-    
-    <script src="<?= $this->_config['js']; ?>/Chart.js"></script>
-	
+    <!--<script src="<?= $this->_config['js']; ?>/Chart.js"></script>  -->
 	<script src="<?= $this->_config['js']; ?>/ChartJs/knockout-3.0.0.js"></script>
 	<script src="<?= $this->_config['js']; ?>/ChartJs/globalize.min.js"></script>
     <script src="<?= $this->_config['js']; ?>/ChartJs/dx.chartjs.js"></script>
-
-    
     <script src="<?= $this->_config['js']; ?>/bootstrap.js"></script>
     <script src="<?= $this->_config['js']; ?>/bootstrap-maxlength.js"></script>
     <script src="<?= $this->_config['js']; ?>/nprogress.js"></script>
     <script src="<?= $this->_config['js']; ?>/prettyCheckable/prettyCheckable.js"></script>
-    
     <script src="<?= $this->_config['js']; ?>/custom.js"></script>
-
-
   </body>
 </html>
